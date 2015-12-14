1a6cfa968fe4454ef444099750b54260e7271c09
vivek maskara <maskaravivek@gmail.com>
2015-12-14 12:08:23 +0530

    added mobile service and code to insert and display data to the table

---
+++ Tuts+ Mobile Service Demo/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.MobileServices;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Tuts__Mobile_Service_Demo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MobileServiceCollection<ToDoItem, ToDoItem> items;
+        private IMobileServiceTable<ToDoItem> todoTable =
+            App.tutsplusdemoClient.GetTable<ToDoItem>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await RefreshTodoItems();
+        }
+
+        private async Task RefreshTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // Query that returns all items.   
+                items = await todoTable.ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                ListItems.ItemsSource = items;
+                this.ButtonSave.IsEnabled = true;
+            }
+        }
+
+        private async void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonRefresh.IsEnabled = false;
+
+            //await SyncAsync(); // offline sync
+            await RefreshTodoItems();
+
+            ButtonRefresh.IsEnabled = true;
+        }
+
+        public async Task InsertToDoItem(ToDoItem toDoItem)
+        {
+            await todoTable.InsertAsync(toDoItem);
+            items.Add(toDoItem);
+        }
+    }
+}
---
+++ Tuts+ Mobile Service Demo/ToDoItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuts__Mobile_Service_Demo
+{
+    public class ToDoItem
+    {
+        public string Id { get; set; }
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "complete")]
+        public bool Complete { get; set; }
+
+        public ToDoItem()
+        {
+            
+        }
+
+        public ToDoItem(string text, bool status = false)
+        {
+            Text = text;
+            Complete = status;
+        }
+    }
+}
